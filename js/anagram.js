fb4b62a98ea7d605cfcf5974fac9b47e29182989
Irechukwu Confidence <irechukwuconfidence@gmail.com>
2021-10-11 11:42:27 +0100

    .

---
+++ js/anagram.js
@@ -0,0 +1,47 @@
+// Write a program in JavaScript to check if two strings are anagrams of each other or not
+// JavascriptWeb DevelopmentFront End Technology
+
+// Given two strings ‘a’ and string ‘b’, we have to check 
+// if they are anagrams of each other or not and return True/False. For example,
+
+// Input-1 −
+
+// String a= “india”
+// String b= “nidia”
+// Output -True
+
+// Explanation − Since the given string ‘b’ contains all 
+// the characters in the string ‘a’ thus we will return True.
+
+// Input-2 −
+
+// String a= “hackathon”
+// String b= “achcthoon”
+// Output −False
+
+// Explanation − Since the given string ‘b’ doesn’t have 
+// all the characters as string ‘a’ have thus we will return False.
+
+
+function checkStringsAnagram(a, b) {
+    // Checking the length
+    let len1 = a.length;
+    let len2 = b.length;
+    if(len1 !== len2){
+       return (console.log('Invalid Input'));
+    }
+
+    //
+    let str1 = a.split('').sort().join('');
+    console.log(str1)
+    let str2 = b.split('').sort().join('');
+    console.log(str2)
+    if(str1 === str2){
+       console.log("True");
+    } else { 
+       console.log("False");
+    }
+ }
+ checkStringsAnagram("indian","ndiani")
+ checkStringsAnagram("hackathon", "achcthoon")
+ checkStringsAnagram("hackathon", "zpbdefg")
